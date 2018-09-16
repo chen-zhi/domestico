dbd83fd6234f5533e66a71dcd88cd71d285a3752
Zhi Chen <chenzhi.finland@gmail.com>
2018-09-16 23:44:37 +0200

    Add Header component
    This uses react-bootstrap

---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from "react-bootstrap";
+import "./Header.css";
+import logoUrl from "../../assets/logo.svg";
+
+const Header = () => (
+  <div className="header">
+    <Navbar>
+      <Navbar.Header>
+        <Navbar.Brand>
+          <a href="#home">Domestico</a>
+          <img className="logo" src={logoUrl} alt="domestico" />
+        </Navbar.Brand>
+      </Navbar.Header>
+      <Nav>
+        <NavItem eventKey={1} href="#">
+          Link
+        </NavItem>
+        <NavItem eventKey={2} href="#">
+          Link
+        </NavItem>
+        <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+          <MenuItem eventKey={3.1}>Action</MenuItem>
+          <MenuItem eventKey={3.2}>Another action</MenuItem>
+          <MenuItem eventKey={3.3}>Something else here</MenuItem>
+          <MenuItem divider />
+          <MenuItem eventKey={3.4}>Separated link</MenuItem>
+        </NavDropdown>
+      </Nav>
+    </Navbar>
+  </div>
+);
+
+export default Header;
